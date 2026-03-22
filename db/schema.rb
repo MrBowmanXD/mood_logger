59ff491736006f226edae153022db4cbd41be655
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2026-03-22 15:11:43 +0000

    Added authentication using the rails 8 generator and sign up logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_22_103723) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_22_144456) do
   create_table "moods", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "emotion"
@@ -27,4 +27,23 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_22_103723) do
     t.string "objective"
     t.datetime "updated_at", null: false
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
